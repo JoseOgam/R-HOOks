6b84f30ee25426588a87265eee2537d80ed37790
JoseOgam <jotienoochieng@gmail.com>
2020-07-16 13:57:52 -0400

    working with hooks in react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var App = props => {
+  var [count, setCount] = useState(props.count)
+  var [text, setText] = useState('')
+  return (
+    <div>
+      <h2>
+        the current {text || 'count'} is {count}
+      </h2>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <input value={text} onChange={e => setText(e.target.value)} />
+    </div>
+  )
+}
+App.defaultProps = {
+  count: 0
+}
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
