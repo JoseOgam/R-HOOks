e68975c6eda1b03cff9ee42a37f0fd7f10c0f026
JoseOgam <jotienoochieng@gmail.com>
2020-07-16 15:54:39 -0400

    working with hooks on react app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+// import "./styles/styles";
+import "./index.css";
 
-var App = props => {
-  var [count, setCount] = useState(props.count)
-  var [text, setText] = useState('')
+var App = (props) => {
+  var [count, setCount] = useState(props.count);
+  var [text, setText] = useState("");
   return (
-    <div>
-      <h2>
-        the current {text || 'count'} is {count}
+    <div className="container">
+      <h2 className="header-text">
+        the current {text || "count"} is {count}
       </h2>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(0)}>reset</button>
-      <input value={text} onChange={e => setText(e.target.value)} />
+      <button className="button" onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button className="button--link" onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+      <button className="button" onClick={() => setCount(0)}>
+        reset
+      </button>
+      <input
+        className="input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 App.defaultProps = {
-  count: 0
-}
-ReactDOM.render(<App />, document.getElementById('root'))
+  count: 0,
+};
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
